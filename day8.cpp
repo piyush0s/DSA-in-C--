95ff730c3fcda0afc3020539741d131cd06229a6
Piyush Sharma <piyushsharm59@gmail.com>
2025-08-03 14:25:43 +0530

    day 8

--- day8.cpp
+++ day8.cpp
@@ -18,3 +18,52 @@ vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
     return result;
 }
 
+
+// 54. Spiral Matrix
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int total = row * col;
+        int count = 0;
+        int startRow = 0;
+        int endRow = row-1;
+        int startCol = 0;
+        int endCol = col -1;
+        
+        while(count < total ){
+            for(int index = startCol ; index <= endCol && count < total ; index ++) {
+                ans.push_back(matrix[startRow][index]);
+                count ++;
+            } 
+            startRow++;
+
+            for(int index = startRow ; index <= endRow && count <total ; index++){
+                ans.push_back(matrix[index][endCol]);
+                    count++;
+                
+            }
+            endCol--;
+
+            for(int index = endCol ; index >= startCol && count < total ; index -- ){
+                ans.push_back(matrix[endRow][index]);
+                count ++;
+            }
+            endRow--;
+
+            for(int index = endRow ; index >= startRow && count < total ; index --){
+                ans.push_back(matrix[index][startCol]);
+                count ++;
+            }
+            startCol++;
+
+
+
+        }
+        return ans;
+        
+    }
+};
