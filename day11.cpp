08bbb949d0a9501daf141fec19a582f9955a5922
Piyush Sharma <piyushsharm59@gmail.com>
2025-08-13 23:49:51 +0530

    day11

--- day11.cpp
+++ day11.cpp
@@ -23,3 +23,31 @@ public:
         return hasVowel && hasConsonant;
     }
 };
+
+// remove duplicates from string 2
+
+
+
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        int n  = s.size();
+         vector<int>count(n);
+        int i = 0;
+        for(int j =0; j<n; j++){
+            s[i]=s[j];
+            if(i > 0 && s[i]==s[i-1])
+              count[i]= count[i-1]+1;
+            else
+            count[i]=1;
+            
+            if(count[i]==k){
+                i-=k;
+            }
+            ++i;
+
+        }
+        return s.substr(0,i);
+        
+    }
+};
