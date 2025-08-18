4de2489bbe780ae408c5e92e5f8b188224cca22d
Piyush Sharma <piyushsharm59@gmail.com>
2025-08-18 20:28:14 +0530

    day14

--- day14.cpp
+++ day14.cpp
@@ -27,3 +27,50 @@ public:
         return s.substr(start,end);
     }
 };
+
+
+
+// 224// LeetCode 224. Basic Calculator
+
+class Solution {
+public:
+    int calculate(string s) {
+        long result = 0;
+        long number = 0;
+        int sign = 1;
+        stack<int> stk;
+
+        for(char c : s){
+            if(isdigit(c)){
+                number = number * 10 +(c- '0');
+            }else if(c == '+'){
+                result+=sign*number;
+                number = 0;
+                sign = 1;
+            }else if(c == '-'){
+                result+= sign*number;
+                number = 0;
+                sign = -1;
+            }else if(c == '('){
+                stk.push(result);
+                stk.push(sign);
+
+                result = 0;
+                sign = 1;
+            }
+            else if(c ==')'){
+                result+= sign*number;
+                number = 0;
+
+                result*=stk.top(); stk.pop();
+                result+=stk.top(); stk.pop();
+
+            }
+
+
+        }
+
+        if(number !=0) return result+=sign *number;
+        return result;
+    }
+};
